565f104115ee929f68fcd30d7e744d06e42abf01
db001 <dnbennett@hotmail.co.uk>
2018-03-31 23:02:41 +0100

    Gallery select buttons added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-bootstrap';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, ButtonGroup, Button } from 'react-bootstrap';
 
+// Hardcoded state
 const images = {
   mri: {
-    folder: "mri/",
     jpgs: [
       'mri1.jpg',
       'mri2.jpg',
@@ -16,7 +16,6 @@ const images = {
     ],
   },
   swarm: {
-    folder: "swarm/",
     jpgs: [
       'swarm1.jpg',
       'swarm2.jpg',
@@ -30,7 +29,6 @@ const images = {
     ],
   },
   installation: {
-    folder: "installation/",
     jpgs: [
       'installation1.jpg',
       'installation2.jpg',
@@ -38,11 +36,11 @@ const images = {
     ]
   },
   baseURL: 'http://res.cloudinary.com/dl9xyhypx/image/upload/',
-  heightModifier: 'h_286',
   imageTransforms: ',c_lpad,b_white/',
   userId: 'v1519334193/',
   windowWidth: 0,
-  imageHeight: 0
+  imageHeight: 0,
+  currentFolder: 'mri'
 };
 
 class Gallery extends Component { 
@@ -52,13 +50,15 @@ class Gallery extends Component {
     this.state = {...images};
     this.getWindowWidth = this.getWindowWidth.bind(this);
     this.setImageHeight = this.setImageHeight.bind(this);
-    
+    this.chooseImageSet = this.chooseImageSet.bind(this);
   }
 
+  // Get the width of the current window
   getWindowWidth() {
     this.setState({windowWidth: Math.max(document.documentElement.clientWidth || 0)})
   }
 
+  // Set height of the gallery images depending on window width
   setImageHeight() {
     this.getWindowWidth();
     if(this.state.windowWidth < 692) {
@@ -68,18 +68,23 @@ class Gallery extends Component {
     } else if(this.state.windowWidth > 992) {
       this.setState({imageHeight: 600});
     }
-    console.log(this.state.windowWidth, this.state.imageHeight);
   }
 
+  // Select images based on button click
+  chooseImageSet(e) {
+    this.setState({currentFolder: e.currentTarget.dataset.id})
+  }
+
+  // Call setImageHeight on component mount and add event listener for window resize
   componentDidMount() {
-    // this.getWindowWidth();
     this.setImageHeight();
     window.addEventListener('resize', this.setImageHeight.bind(this));
   }
 
   render() {
     const imgs = this.state;
-    const imagesURL = `${imgs.baseURL}h_${imgs.imageHeight}${imgs.imageTransforms}${imgs.mri.folder}`;
+    const galleryToShow = imgs.currentFolder;
+    const imagesURL = `${imgs.baseURL}h_${imgs.imageHeight}${imgs.imageTransforms}${imgs.currentFolder}/`;
 
     return (
       <div className="gallery">
@@ -98,19 +103,20 @@ class Gallery extends Component {
                       <img alt={image} src={imagesURL + image} />
                       <Carousel.Caption>
                       <h3>{image}</h3>
-                      {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
                       </Carousel.Caption>
                     </Carousel.Item>
                   )
                 }
-                {/* <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src="http://via.placeholder.com/900x500" />
-                  <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Carousel.Caption>
-                </Carousel.Item> */}
               </Carousel>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <ButtonGroup bsSize="large">
+                <Button data-id="mri" onClick={this.chooseImageSet}>MRI</Button>
+                <Button data-id="swarm" onClick={this.chooseImageSet}>Swarm</Button>
+                <Button data-id="install" onClick={this.chooseImageSet}>Installation</Button>
+              </ButtonGroup>
             </Col>
           </Row>
         </Grid>
