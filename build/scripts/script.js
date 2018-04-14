be7533259e881b0990f83f6aec883716db1aaa9b
db001 <dnbennett@hotmail.co.uk>
2018-04-14 21:15:54 +0100

    Build folder updated

---
+++ build/scripts/script.js
@@ -0,0 +1,13 @@
+function fadeInNav() {
+  const navbar = document.querySelector('.navbar');
+  const about = document.querySelector('.about');
+  const fadeInPoint = about.getBoundingClientRect().top - 200;
+
+  if(fadeInPoint < 0) {
+    navbar.style.background = `rgba(110, 182, 241, ${Math.abs(fadeInPoint / 100)})`;
+  } else {
+    navbar.style.background = `rgba(110, 182, 241, 0.0)`;    
+  }
+}
+
+window.addEventListener('scroll', fadeInNav);
