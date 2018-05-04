57210a9ef20e348847e616daf938165111a9f405
db001 <dnbennett@hotmail.co.uk>
2018-05-04 21:05:18 +0100

    new images added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -33,12 +33,15 @@ const images = {
     jpgs: [
       'installation1.jpg',
       'installation2.jpg',
-      'installation3.jpg'
+      'installation3.jpg',
+      'installation4.jpg',
+      'installation5.jpg',
+      'installation6.jpg'
     ]
   }
 }
 
-const gallerySettings= {
+const gallerySettings = {
   baseURL: 'http://res.cloudinary.com/dl9xyhypx/image/upload/',
   imageTransforms: ',c_lpad,b_white/',
   userId: 'v1519334193/',
@@ -47,36 +50,36 @@ const gallerySettings= {
   currentFolder: 'mri'
 };
 
-class Gallery extends Component { 
+class Gallery extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {images, ...gallerySettings};
+    this.state = { images, ...gallerySettings };
     this.getWindowWidth = this.getWindowWidth.bind(this);
     this.setImageHeight = this.setImageHeight.bind(this);
-    this.chooseImageSet = this.chooseImageSet.bind(this); 
+    this.chooseImageSet = this.chooseImageSet.bind(this);
   }
 
   // Get the width of the current window
   getWindowWidth() {
-    this.setState({windowWidth: Math.max(document.documentElement.clientWidth || 0)})
+    this.setState({ windowWidth: Math.max(document.documentElement.clientWidth || 0) })
   }
 
   // Set height of the gallery images depending on window width
   setImageHeight() {
     this.getWindowWidth();
-    if(this.state.windowWidth < 692) {
-      this.setState({imageHeight: 275});
-    } else if(this.state.windowWidth >= 692 && this.state.windowWidth < 992) {
-      this.setState({imageHeight: 450});
-    } else if(this.state.windowWidth > 992) {
-      this.setState({imageHeight: 600});
+    if (this.state.windowWidth < 692) {
+      this.setState({ imageHeight: 275 });
+    } else if (this.state.windowWidth >= 692 && this.state.windowWidth < 992) {
+      this.setState({ imageHeight: 450 });
+    } else if (this.state.windowWidth > 992) {
+      this.setState({ imageHeight: 600 });
     }
   }
 
   // Select images based on button click
   chooseImageSet(e) {
-    this.setState({currentFolder: e.currentTarget.dataset.id})
+    this.setState({ currentFolder: e.currentTarget.dataset.id })
   }
 
   // Call setImageHeight on component mount and add event listener for window resize
@@ -84,49 +87,49 @@ class Gallery extends Component {
     this.getWindowWidth();
     this.setImageHeight();
     window.addEventListener('resize', this.setImageHeight.bind(this));
-    window.addEventListener('load', this.setImageHeight.bind(this));  
+    window.addEventListener('load', this.setImageHeight.bind(this));
   }
 
-  render() { 
-    
+  render() {
+
     const imagesURL = `http://res.cloudinary.com/dl9xyhypx/image/upload/h_${this.state.imageHeight},c_lpad,c_lfill,b_white/v1519334193/${this.state.currentFolder}/`;
-    const imageFolder = {...this.state.images[this.state.currentFolder]};
+    const imageFolder = { ...this.state.images[this.state.currentFolder] };
 
     return (
       <div className="gallery">
         <ScrollableAnchor id={'gallery'}>
-        <Grid>
-          <Row>
-            <Col xs={12}>
+          <Grid>
+            <Row>
+              <Col xs={12}>
                 <h3>Gallery</h3>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <Carousel>
-                {
-                  imageFolder.jpgs.map(image =>
-                    <Carousel.Item className="paintingCarousel" key={image}>
-                      <img alt={image} src={imagesURL + image} />
-                      {/* <Carousel.Caption>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12}>
+                <Carousel>
+                  {
+                    imageFolder.jpgs.map(image =>
+                      <Carousel.Item className="paintingCarousel" key={image}>
+                        <img alt={image} src={imagesURL + image} />
+                        {/* <Carousel.Caption>
                         <h3>{image}</h3>
                       </Carousel.Caption> */}
-                    </Carousel.Item>
-                  )
-                }
-              </Carousel>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12}>
-              <ButtonGroup bsSize="large">
-                <Button data-id="mri" onClick={this.chooseImageSet}>MRI</Button>
-                <Button data-id="swarm" onClick={this.chooseImageSet}>Swarm</Button>
-                <Button data-id="installation" onClick={this.chooseImageSet}>Installation</Button>
-              </ButtonGroup>
-            </Col>
-          </Row>
-        </Grid>
+                      </Carousel.Item>
+                    )
+                  }
+                </Carousel>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12}>
+                <ButtonGroup bsSize="large">
+                  <Button data-id="mri" onClick={this.chooseImageSet}>MRI</Button>
+                  <Button data-id="swarm" onClick={this.chooseImageSet}>Swarm</Button>
+                  <Button data-id="installation" onClick={this.chooseImageSet}>Installation</Button>
+                </ButtonGroup>
+              </Col>
+            </Row>
+          </Grid>
         </ScrollableAnchor>
       </div>
     )
